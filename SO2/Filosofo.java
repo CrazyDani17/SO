ad7bf46aaf35fffd2904bccd01976686d4cbc7e5
MarvikDC <42683863+MarvikDC@users.noreply.github.com>
2020-05-17 18:18:16 -0500

    Create Filosofo.java

---
+++ SO2/Filosofo.java
@@ -0,0 +1,76 @@
+package so2;
+
+public class Filosofo extends Thread {
+	
+	private int estomago;
+	private int nfilosofo;
+	private boolean [] turnos;
+	
+	protected Filosofo(int n, boolean[] t) {
+        this.nfilosofo = n;
+        this.turnos=t;
+        this.estomago = 20;
+    }
+	
+	public void run() {
+		while(true) {
+			comer();
+			pensar();
+			if(this.estomago<0) {
+				System.out.println(this.getName() + " acabo de morir de hambre.");
+			}
+		}
+	}
+	
+	@SuppressWarnings("static-access")
+	public void comer() {
+		if(this.turnos[this.nfilosofo]) {
+			long i;
+			long inicio = System.nanoTime();
+			System.out.println("");
+			System.out.println("Filósofo " + this.getName() + " está comiendo.");
+			System.out.println("");
+			try {
+				this.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("+Se ha producido una excepcion...");
+			}
+			long despues = System.nanoTime();
+			long tiempo = despues - inicio;
+			i=tiempo%7;
+			this.estomago+=i;
+			this.turnos[this.nfilosofo]=false;
+			if(this.nfilosofo!=this.turnos.length-1) {
+				this.turnos[this.nfilosofo + 1]=true;
+			}
+			else{
+				this.turnos[0]=true;
+			}
+		}
+		else { 
+            System.out.println("Filósofo " + this.getName() + " está esperando."); 
+        }
+	}
+	@SuppressWarnings("static-access")
+	public void pensar() {
+		long i;
+		long inicio = System.nanoTime();
+		try {
+        	System.out.println("Filósofo " + this.getName() + " está pensando."+ this.estomago);
+            this.sleep(1000);
+        } catch (InterruptedException ex) {
+        	// TODO Auto-generated catch block
+        	System.out.println("+Se ha producido una excepcion...");
+        }
+		long despues = System.nanoTime();
+		long tiempo = despues - inicio;
+		i=tiempo%7;
+		while(i>0) {
+			this.estomago--;
+			i--;
+		}
+	}
+
+}
